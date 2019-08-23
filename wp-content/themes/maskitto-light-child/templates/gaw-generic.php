432b7a6538964fb0feea3ee9e6ce60ab8e78ddde
Meshack <mvallesillas@support.ucla.edu>
2019-08-23 12:02:34 -0700

    update UCLA logo

--- wp-content/themes/maskitto-light-child/templates/gaw-generic.php
+++ wp-content/themes/maskitto-light-child/templates/gaw-generic.php
@@ -38,10 +38,9 @@
 </div>
 
 
-
 <div class="logo_container">
     <div class="school_logo">
-        <img src="https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-300.jpg" srcset="https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-120.jpg 120w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-180.jpg 180w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-300.jpg 300w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-600.jpg 600w" alt="UCLA school logo" />
+        <img src="https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-600.png" srcset="https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-120.png 120w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-180.png 180w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-300.png 300w, https://uclafund.assets.support2.ucla.edu/2019/01/ucla-logo-600.png 600w" alt="UCLA school logo" />
     </div> 
 </div>
 
