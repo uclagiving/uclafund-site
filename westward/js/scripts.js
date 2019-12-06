aec8b6ef31527919a7d6a35c5ca054fccd6c8825
Meshack <mvallesillas@support.ucla.edu>
2019-12-06 10:58:48 -0800

    Copy change and timer reset

--- westward/js/scripts.js
+++ westward/js/scripts.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     https://wplearninglab.com/countdown-timer/
     =============================== */
     //var deadline = 'July 1 2019 24:00:00 GMT-0700';
-    var deadline = new Date(2019, 10, 3, 24, 0, 0); 
+    var deadline = new Date(2020, 0, 5, 24, 0, 0); 
 
     function getTimeRemaining(endtime){
         var t = Date.parse(endtime) - Date.parse(new Date());
