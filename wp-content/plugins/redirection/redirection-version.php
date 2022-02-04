3223a78ef47d402876235884af2886d3ec39af08
Silas Lam <slam@support.ucla.edu>
2022-02-03 16:20:20 -0800

    Updating Redirection to 5.2.2

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.1.1' );
-define( 'REDIRECTION_BUILD', 'accf9923c797504c858f60338da73667' );
+define( 'REDIRECTION_VERSION', '5.2.2' );
+define( 'REDIRECTION_BUILD', 'e97dc2feda27f080b79bb711d176f7aa' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
