beab95761a29ec41e0efb935d102ae4d88b53b29
Silas Lam <slam@support.ucla.edu>
2025-10-13 16:13:42 -0700

    Updating WordPress Core and plugins

--- .platform/hooks/postdeploy/plugins.sh
+++ .platform/hooks/postdeploy/plugins.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 curl -LO "https://downloads.wordpress.org/plugin/wp-ses.1.7.2.zip"
-curl -LO "https://downloads.wordpress.org/plugin/all-in-one-wp-security-and-firewall.5.4.2.zip"
+curl -LO "https://downloads.wordpress.org/plugin/all-in-one-wp-security-and-firewall.5.4.3.zip"
 curl -LO "https://downloads.wordpress.org/plugin/404-to-301.3.1.4.zip"
 curl -LO "https://downloads.wordpress.org/plugin/black-studio-tinymce-widget.2.7.3.zip"
-curl -LO "https://downloads.wordpress.org/plugin/google-analytics-for-wordpress.9.6.1.zip"
+curl -LO "https://downloads.wordpress.org/plugin/google-analytics-for-wordpress.9.8.0.zip"
 curl -LO "https://downloads.wordpress.org/plugin/ilab-media-tools.4.6.4.zip"
-curl -LO "https://downloads.wordpress.org/plugin/siteorigin-panels.2.32.1.zip"
-curl -LO "https://downloads.wordpress.org/plugin/post-types-order.2.3.7.zip"
+curl -LO "https://downloads.wordpress.org/plugin/siteorigin-panels.2.33.3.zip"
+curl -LO "https://downloads.wordpress.org/plugin/post-types-order.zip"
 curl -LO "https://downloads.wordpress.org/plugin/redirection.5.5.2.zip"
-curl -LO "https://downloads.wordpress.org/plugin/redux-framework.4.5.7.zip"
+curl -LO "https://downloads.wordpress.org/plugin/redux-framework.4.5.8.zip"
 curl -LO "https://downloads.wordpress.org/plugin/remove-protected-in-title.zip"
-curl -LO "https://downloads.wordpress.org/plugin/so-widgets-bundle.1.69.1.zip"
+curl -LO "https://downloads.wordpress.org/plugin/so-widgets-bundle.1.69.3.zip"
 curl -LO "https://downloads.wordpress.org/plugin/smart-passworded-pages.zip"
-curl -LO "https://downloads.wordpress.org/plugin/the-events-calendar.6.14.1.zip"
-curl -LO "https://downloads.wordpress.org/plugin/insert-headers-and-footers.2.2.9.zip"
+curl -LO "https://downloads.wordpress.org/plugin/the-events-calendar.6.15.7.zip"
+curl -LO "https://downloads.wordpress.org/plugin/insert-headers-and-footers.2.3.0.zip"
 curl -LO "https://downloads.wordpress.org/plugin/duplicate-post.4.5.zip"
 
 unzip -qq -d wp-content/plugins/ \*.zip
